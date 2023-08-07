0dea8863c2d1310a8844f2c195a88fed5ff62cc2
walidsoufiani <140754926+walidsoufiani@users.noreply.github.com>
2023-08-07 12:15:01 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import PyPDF2
-from PyPDF2 import PdfReader
 
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
