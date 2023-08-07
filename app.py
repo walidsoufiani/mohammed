4ecf609fd789a27bf89acb3f763d8086762ae260
walidsoufiani <140754926+walidsoufiani@users.noreply.github.com>
2023-08-07 12:02:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import PyPDF2
 from PyPDF2 import PdfReader
+
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.vectorstores import ElasticVectorSearch, Pinecone, Weaviate, FAISS
